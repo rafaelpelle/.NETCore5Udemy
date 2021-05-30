8639ab184c4d37d3b8661959a418666ea990cfbb
Rafael Pelle <rafael.pelle@supero.com.br>
2021-05-30 18:18:29 -0300

    Seção 7: Conhecendendo os principais verbos

---
+++ 03_.NETCore5Udemy_Verbos/NETCore5Udemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,60 @@
+﻿using NETCore5Udemy.Model;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace NETCore5Udemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private volatile int count;
+
+        public Person Create(Person person)
+        {
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            
+        }
+
+        public List<Person> FindAll()
+        {
+            List<Person> people = new List<Person>();
+            for (int i = 0; i < 10; i++)
+            {
+                Person person = MockPerson();
+                people.Add(person);
+            }
+            return people;
+        }
+
+        public Person FindByID(long id)
+        {
+            Person person = MockPerson();
+            return person;
+        }
+
+        public Person Update(Person person)
+        {
+            return person;
+        }
+
+        private Person MockPerson()
+        {
+            return new Person
+            {
+                Id = IncrementAndGet(),
+                FirstName = "Person"+count,
+                LastName = "Last Name",
+                Address = "Florianópolis - SC",
+                Gender = "Male"
+            };
+        }
+
+        private long IncrementAndGet()
+        {
+            return Interlocked.Increment(ref count);
+        }
+    }
+}
