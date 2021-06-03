21067cdf7074450a0617d7e5ce849356e2ba5dde
Rafael Pelle <rafael.pelle@supero.com.br>
2021-06-03 16:33:57 -0300

    Seção 8: Integrando a Aplicação ao Banco de Dados

---
+++ 04_.NETCore5Udemy_MySQL/NETCore5Udemy/Controllers/PersonController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NETCore5Udemy.Model;
+using NETCore5Udemy.Services.Implementations;
+
+namespace NETCore5Udemy.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            Person person = _personService.FindByID(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            Person result = _personService.Update(person);
+            if (result == null) return NotFound();
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
---
+++ 04_.NETCore5Udemy_MySQL/NETCore5Udemy/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,86 @@
+﻿using NETCore5Udemy.Model;
+using NETCore5Udemy.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace NETCore5Udemy.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private MySQLContext _context;
+
+        public PersonServiceImplementation(MySQLContext context)
+        {
+            _context = context;
+        }
+
+
+        // Get all registered Person
+        public List<Person> FindAll()
+        {
+            return _context.Persons.ToList();
+        }
+
+        // Get a single registered Person
+        public Person FindByID(long id)
+        {
+            Person person = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            return person;
+        }
+
+        // Create a single registered Person
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            } catch (Exception)
+            {
+                throw;
+            }
+            return person;
+        }
+
+
+        // Update a single registered Person
+        public Person Update(Person person)
+        {
+            Person result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+            if (result == null) return null;
+
+            try
+            {
+                _context.Entry(result).CurrentValues.SetValues(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return person;
+        }
+
+
+        // Delete a single registered Person
+        public void Delete(long id)
+        {
+            Person result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+    }
+}
